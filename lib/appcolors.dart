27cd24e0eb2196ca727de7d93544f7ad9897b38f
HimaYoussef <ebr.you2014@gmail.com>
2023-12-12 14:41:13 +0200

    Testing App

--- lib/appcolors.dart
+++ lib/appcolors.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color scaffoldBg = const Color(0xff0a0c21);
-  static Color containerBg = const Color(0xff1d1e33);
-  static Color grey = const Color(0xff4c4f5e);
-  static Color red = const Color(0xffeb1555);
-  static Color green = const Color(0xff0bab63);
+  static Color primaryColor = Colors.blue;
+  static Color black = Colors.black;
   static Color white = Colors.white;
 }
