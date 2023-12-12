d4e786a58ef339738f80e0d6da708e2545bade11
HimaYoussef <ebr.you2014@gmail.com>
2023-12-12 14:44:46 +0200

    Testing App

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/bmi_calculator.dart';
+import 'package:flutter_application_1/Log_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: LoginView(),
     );
   }
 }
